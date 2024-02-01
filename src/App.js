c0765e5c51cfee3f6d660848eca7a505097f3b84
Pasupuleti Arun <arunkumar.pasupuleti774@gmail.com>
2024-02-01 16:41:53 +0530

    upto state and events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PropTypes from "prop-types";
+import pokemon from "./pokemon.json";
+import { useState } from "react";
 
-function App() {
+const PokemonRow = ({ pokemon, onSelect }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr key={pokemon.id}>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(", ")}</td>
+      <td>
+        <button onClick={() => onSelect(pokemon)}>Select</button>
+      </td>
+    </tr>
+  );
+};
+const PokemonInfo = ({ name, base }) => {
+  return (
+    <div>
+      <h1>{name.english}</h1>
+      <table>
+        <tbody>
+          {Object.keys(base).map((key) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{base[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string.isRequired,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string.isRequired,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string.isRequired),
+    onSelect: PropTypes.func.isRequired,
+  }),
+};
+function App() {
+  const [filter, setFilter] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
+  return (
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          columnGap: "15px",
+        }}
+      >
+        <div>
+          <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+          <table width="100%">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Type</td>
+              </tr>
+            </thead>
+            <tbody>
+              {/* here with map we have many values so we can use slice */}
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
+                .slice(0, 20)
+                .map((pokemon) => {
+                  return (
+                    <PokemonRow
+                      pokemon={pokemon}
+                      key={pokemon.id}
+                      onSelect={(pokemon) => setSelectedItem(pokemon)}
+                    />
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </div>
     </div>
   );
 }
